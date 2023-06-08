0f99f12c4cf879ce6fbbfdd931b10886c2dec34e
YehonatamBinyamini <yonile2106@gmail.com>
2023-06-08 13:00:34 +0300

    adding a bottom board

--- src/components/rootLayout/RootLayout.js
+++ src/components/rootLayout/RootLayout.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../header/Header";
+import './rootLayout.css';
 
 export default function RootLayout() {
   return (
     <>
       <Header />
       <Outlet />
+      <div className="bottomBoard">
+        <h4>EasyIron</h4>
+        <p>הזמנת ברזל בקליק</p>
+      </div>
     </>
   );
 }
