da8653a97e8ba3cb26a13c6f2e0a74dde9f30b5e
YehonatamBinyamini <yonile2106@gmail.com>
2023-06-13 23:52:50 +0300

    header fixing

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./header.css";
-import logo from "../../assets/images/1.png";
+import logo from "../../assets/images/white-logo.png";
 import { Link, NavLink } from "react-router-dom";
 function Header(props) {
   //   const activeColor = "rgb(241, 213, 56)";
@@ -18,20 +18,22 @@ function Header(props) {
 
   return (
     <header className="header">
-      <Link to="./">
-        <img className="logo" src={logo} alt="logo" />
-      </Link>
-      <nav className="nav">
+      <div className="connect-header">
         <label className="userName-header">שלום {userName}</label>
         <label className="userName-header">|</label>
         <NavLink to="./login" onClick={loginHandler}>
           התחבר
         </NavLink>
+      </div>
+      <nav className="nav">
         <NavLink to="./NewOrder">צור הזמנה</NavLink>
         <NavLink to="./Catalog">קטלוג</NavLink>
         <NavLink to="./About">אודות</NavLink>
         <NavLink to="./ContactUs">צור קשר</NavLink>
       </nav>
+      <Link to="./">
+        <img className="logo" src={logo} alt="logo" />
+      </Link>
     </header>
   );
 }
