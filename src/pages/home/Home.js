b5f0ade0f57c9b664f61efbc18c7576387829a50
YehonatamBinyamini <yonile2106@gmail.com>
2023-06-21 23:13:00 +0300

    new design for home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,20 +1,32 @@
 import React from "react";
 import "./home.css";
-import movie from "../../assets/images/metal-video.mp4";
+import { FaStar, FaCircle, FaArrowAltCircleRight } from "react-icons/fa";
+//import movie from "../../assets/images/metal-video.mp4";
 
 function Home() {
   return (
-    <div className="home-container">
-      <h1>EasyIron</h1>
-      <video
-        loop={true}
-        muted={true}
-        src={movie}
-        autoPlay={true}
-        id={"metal-video"}
-      >
-      </video>
-    </div>
+    <>
+    <h1>easyiron</h1>
+    <div className="vertical-line">
+  <div className="line"></div>
+  <div className="point">
+    {/* <img className="icon" src={constructionIcon} alt="Construction Icon" /> */}
+    <span>Start</span>
+  </div>
+  <div className="point">
+    {/* <img className="icon" src={icon2} alt="Icon 2" /> */}
+    <span>Point 2</span>
+  </div>
+  <div className="point">
+    {/* <img className="icon" src={icon3} alt="Icon 3" /> */}
+    <span>Point 3</span>
+  </div>
+  <div className="point">
+    {/* <img className="icon" src={icon4} alt="Icon 4" /> */}
+    <span>End</span>
+  </div>
+</div>
+</>
   );
 }
 
